1005acef851b48db8bc944505e252612f5b5ac6a
rezaramadhani <rezarmd17@gmail.com>
2022-10-30 21:47:50 +0700

    membuat file controller

---
+++ src/main/java/com/example/demo/tableController.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.demo;
+
+/**
+ *
+ * @author Reza Ramadhani
+ */
+public class tableController {
+    
+}
